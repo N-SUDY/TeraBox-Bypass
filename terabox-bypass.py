a1da3ac6fb22674e7b592094781b60689305db1f
oxhellfire <0xhellfire@gmail.com>
2023-08-29 14:30:21 +0530

    if there is no args then use input

--- terabox-bypass.py
+++ terabox-bypass.py
@@ -6,8 +6,10 @@ from humanize import naturalsize
 from urllib.parse import quote, unquote
 
 
-# terabox_url = "https://teraboxapp.com/s/13FlMJT4cytJbA00IBUWX0w" # file with directories
-# terabox_url = "https://terabox.app/s/16oyv6pH-e97aYGPrLpbTTQ" # only file
+# sample_url = "https://teraboxapp.com/s/13FlMJT4cytJbA00IBUWX0w" # file with directories
+# sample_url = "https://terabox.app/s/16oyv6pH-e97aYGPrLpbTTQ" # only file
+
+terabox_url = terabox_url = input("Terabox url: ") if len(argv) < 2 else argv[1]
 
 json_data_url = "https://www.4funbox.com/share/list?jsToken={jsToken}&shorturl={key}"
 cookies = {"ndus": "YbDgQCEteHui0Bx8sPAmBS3hSB4K79edBrj6PrJq"}
@@ -50,7 +52,7 @@ def bypass_directory_logic(jsToken, key, link, cookies, depth=0):
                 bypass_directory_logic(jsToken, key, sub_link, cookies, depth + 1)
 
 
-process_terabox(argv[1])
+process_terabox(terabox_url)
 
 base = json_data_url.format(jsToken=jsToken, key=key)
 res = requests.get(f"{base}&root=1", cookies=cookies)
